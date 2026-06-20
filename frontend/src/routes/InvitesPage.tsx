85e7ab2029dd696d4e5cdcab7ddf502ef3520217
Daniel Gnägi <daniel.gnaegi@digitecgalaxus.ch>
2026-06-20 19:40:04 +0200

    fix invitee

--- frontend/src/routes/InvitesPage.tsx
+++ frontend/src/routes/InvitesPage.tsx
@@ -6,7 +6,7 @@ import { useAuth } from '../auth/AuthContext'
 import { PageWrapper, PageTitle, Button } from '../components/Layout.styled'
 import { InviteCard, InviteCode, InviteStatus, InviteActions, TopBar } from './InvitesPage.styled'
 
-const getInviteBase = () => `${window.location.origin}/offers?invite=`
+const getInviteBase = () => `${window.location.origin}/register?invite=`
 
 export function InvitesPage() {
   const { t } = useTranslation(['invites', 'common'])
